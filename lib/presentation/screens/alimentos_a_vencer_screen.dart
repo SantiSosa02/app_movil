597af83e1d437f43f51f6a2d939487c8021f2c27
PaulaNino <paulaa_ninob@soy.sea.edu.co>
2023-09-07 06:22:26 -0500

    pantallaAlimentosVencer

---
+++ lib/presentation/screens/alimentos_a_vencer_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlimentosAVencer extends StatefulWidget {
+  const AlimentosAVencer({super.key});
+
+  @override
+  State<AlimentosAVencer> createState() => _AlimentosAVencerState();
+}
+
+class _AlimentosAVencerState extends State<AlimentosAVencer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
